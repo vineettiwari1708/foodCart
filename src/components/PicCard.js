623c40351ba57dfab478e786c80ca87df0f066c1
unknown <vineettiwari1708@gmail.com>
2024-05-19 14:42:49 +0530

    add_star_timming

--- src/components/PicCard.js
+++ src/components/PicCard.js
@@ -5,8 +5,8 @@ const PicCard = (props) => {
       <img className="img-container" src={picCard.url}></img>
       <h3>{picCard.title}</h3>
       <h4>{picCard.id}</h4>
-      <h4>4.5 stars</h4>
-      <h4>38 mins</h4>
+      <h4>{picCard.rating}</h4>
+      <h4>{picCard.timming}</h4>
     </div>
   );
 };
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -5,6 +5,8 @@ const picCards = [
     title: "accusamus beatae ad facilis cum similique qui sunt",
     url: "https://via.placeholder.com/600/92c952",
     thumbnailUrl: "https://via.placeholder.com/150/92c952",
+    rating: 4.5,
+    timming: "38 min",
   },
   {
     albumId: 1,
@@ -12,6 +14,8 @@ const picCards = [
     title: "reprehenderit est deserunt velit ipsam",
     url: "https://via.placeholder.com/600/771796",
     thumbnailUrl: "https://via.placeholder.com/150/771796",
+    rating: 3.2,
+    timming: "28 min",
   },
   {
     albumId: 1,
@@ -19,6 +23,8 @@ const picCards = [
     title: "officia porro iure quia iusto qui ipsa ut modi",
     url: "https://via.placeholder.com/600/24f355",
     thumbnailUrl: "https://via.placeholder.com/150/24f355",
+    rating: 4.0,
+    timming: "15 min",
   },
   {
     albumId: 1,
@@ -26,6 +32,8 @@ const picCards = [
     title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
     url: "https://via.placeholder.com/600/d32776",
     thumbnailUrl: "https://via.placeholder.com/150/d32776",
+    rating: 4.7,
+    timming: "20 min",
   },
   {
     albumId: 1,
@@ -33,6 +41,8 @@ const picCards = [
     title: "natus nisi omnis corporis facere molestiae rerum in",
     url: "https://via.placeholder.com/600/f66b97",
     thumbnailUrl: "https://via.placeholder.com/150/f66b97",
+    rating: 4.9,
+    timming: "40 min",
   },
   {
     albumId: 1,
@@ -40,6 +50,8 @@ const picCards = [
     title: "accusamus ea aliquid et amet sequi nemo",
     url: "https://via.placeholder.com/600/56a8c2",
     thumbnailUrl: "https://via.placeholder.com/150/56a8c2",
+    rating: 2.2,
+    timming: "30 min",
   },
   {
     albumId: 1,
@@ -47,6 +59,8 @@ const picCards = [
     title: "officia delectus consequatur vero aut veniam explicabo molestias",
     url: "https://via.placeholder.com/600/b0f7cc",
     thumbnailUrl: "https://via.placeholder.com/150/b0f7cc",
+    rating: 3.8,
+    timming: "35 min",
   },
   {
     albumId: 1,
@@ -54,6 +68,8 @@ const picCards = [
     title: "aut porro officiis laborum odit ea laudantium corporis",
     url: "https://via.placeholder.com/600/54176f",
     thumbnailUrl: "https://via.placeholder.com/150/54176f",
+    rating: 2.1,
+    timming: "22 min",
   },
   {
     albumId: 1,
@@ -61,6 +77,8 @@ const picCards = [
     title: "qui eius qui autem sed",
     url: "https://via.placeholder.com/600/51aa97",
     thumbnailUrl: "https://via.placeholder.com/150/51aa97",
+    rating: 4.2,
+    timming: "25 min",
   },
   {
     albumId: 1,
@@ -68,6 +86,8 @@ const picCards = [
     title: "beatae et provident et ut vel",
     url: "https://via.placeholder.com/600/810b14",
     thumbnailUrl: "https://via.placeholder.com/150/810b14",
+    rating: 3.2,
+    timming: "27 min",
   },
   {
     albumId: 1,
@@ -75,6 +95,8 @@ const picCards = [
     title: "nihil at amet non hic quia qui",
     url: "https://via.placeholder.com/600/1ee8a4",
     thumbnailUrl: "https://via.placeholder.com/150/1ee8a4",
+    rating: 3.8,
+    timming: "20 min",
   },
   {
     albumId: 1,
@@ -83,6 +105,8 @@ const picCards = [
       "mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores",
     url: "https://via.placeholder.com/600/66b7d2",
     thumbnailUrl: "https://via.placeholder.com/150/66b7d2",
+    rating: 3.3,
+    timming: "16 min",
   },
   {
     albumId: 1,
@@ -90,6 +114,8 @@ const picCards = [
     title: "repudiandae iusto deleniti rerum",
     url: "https://via.placeholder.com/600/197d29",
     thumbnailUrl: "https://via.placeholder.com/150/197d29",
+    rating: 3.0,
+    timming: "45 min",
   },
   {
     albumId: 1,
@@ -97,6 +123,8 @@ const picCards = [
     title: "est necessitatibus architecto ut laborum",
     url: "https://via.placeholder.com/600/61a65",
     thumbnailUrl: "https://via.placeholder.com/150/61a65",
+    rating: 3.9,
+    timming: "40 min",
   },
   {
     albumId: 1,
@@ -104,6 +132,8 @@ const picCards = [
     title: "harum dicta similique quis dolore earum ex qui",
     url: "https://via.placeholder.com/600/f9cee5",
     thumbnailUrl: "https://via.placeholder.com/150/f9cee5",
+    rating: 4.3,
+    timming: "22 min",
   },
   {
     albumId: 1,
@@ -111,6 +141,8 @@ const picCards = [
     title: "iusto sunt nobis quasi veritatis quas expedita voluptatum deserunt",
     url: "https://via.placeholder.com/600/fdf73e",
     thumbnailUrl: "https://via.placeholder.com/150/fdf73e",
+    rating: 4.2,
+    timming: "21 min",
   },
   {
     albumId: 1,
@@ -118,6 +150,8 @@ const picCards = [
     title: "natus doloribus necessitatibus ipsa",
     url: "https://via.placeholder.com/600/9c184f",
     thumbnailUrl: "https://via.placeholder.com/150/9c184f",
+    rating: 3.5,
+    timming: "33 min",
   },
   {
     albumId: 1,
@@ -125,6 +159,8 @@ const picCards = [
     title: "laboriosam odit nam necessitatibus et illum dolores reiciendis",
     url: "https://via.placeholder.com/600/1fe46f",
     thumbnailUrl: "https://via.placeholder.com/150/1fe46f",
+    rating: 3.7,
+    timming: "39 min",
   },
   {
     albumId: 1,
@@ -132,6 +168,8 @@ const picCards = [
     title: "perferendis nesciunt eveniet et optio a",
     url: "https://via.placeholder.com/600/56acb2",
     thumbnailUrl: "https://via.placeholder.com/150/56acb2",
+    rating: 4.15,
+    timming: "31 min",
   },
   {
     albumId: 1,
@@ -140,6 +178,8 @@ const picCards = [
       "assumenda voluptatem laboriosam enim consequatur veniam placeat reiciendis error",
     url: "https://via.placeholder.com/600/8985dc",
     thumbnailUrl: "https://via.placeholder.com/150/8985dc",
+    rating: 4.82,
+    timming: "27 min",
   },
   {
     albumId: 1,
@@ -147,6 +187,8 @@ const picCards = [
     title: "ad et natus qui",
     url: "https://via.placeholder.com/600/5e12c6",
     thumbnailUrl: "https://via.placeholder.com/150/5e12c6",
+    rating: 3.22,
+    timming: "42 min",
   },
   {
     albumId: 1,
@@ -154,6 +196,8 @@ const picCards = [
     title: "et ea illo et sit voluptas animi blanditiis porro",
     url: "https://via.placeholder.com/600/45601a",
     thumbnailUrl: "https://via.placeholder.com/150/45601a",
+    rating: 2.55,
+    timming: "47 min",
   },
   {
     albumId: 1,
@@ -161,6 +205,8 @@ const picCards = [
     title: "harum velit vero totam",
     url: "https://via.placeholder.com/600/e924e6",
     thumbnailUrl: "https://via.placeholder.com/150/e924e6",
+    rating: 2.8,
+    timming: "25 min",
   },
   {
     albumId: 1,
@@ -168,6 +214,8 @@ const picCards = [
     title: "beatae officiis ut aut",
     url: "https://via.placeholder.com/600/8f209a",
     thumbnailUrl: "https://via.placeholder.com/150/8f209a",
+    rating: 1.5,
+    timming: "23 min",
   },
   {
     albumId: 1,
@@ -175,6 +223,8 @@ const picCards = [
     title: "facere non quis fuga fugit vitae",
     url: "https://via.placeholder.com/600/5e3a73",
     thumbnailUrl: "https://via.placeholder.com/150/5e3a73",
+    rating: 1.7,
+    timming: "21 min",
   },
   {
     albumId: 1,
@@ -182,6 +232,8 @@ const picCards = [
     title: "asperiores nobis voluptate qui",
     url: "https://via.placeholder.com/600/474645",
     thumbnailUrl: "https://via.placeholder.com/150/474645",
+    rating: 1.9,
+    timming: "32 min",
   },
   {
     albumId: 1,
@@ -189,6 +241,8 @@ const picCards = [
     title: "sit asperiores est quos quis nisi veniam error",
     url: "https://via.placeholder.com/600/c984bf",
     thumbnailUrl: "https://via.placeholder.com/150/c984bf",
+    rating: 2.1,
+    timming: "39 min",
   },
   {
     albumId: 1,
@@ -196,6 +250,8 @@ const picCards = [
     title: "non neque eligendi molestiae repudiandae illum voluptatem qui aut",
     url: "https://via.placeholder.com/600/392537",
     thumbnailUrl: "https://via.placeholder.com/150/392537",
+    rating: 2.7,
+    timming: "35 min",
   },
   {
     albumId: 1,
@@ -203,6 +259,8 @@ const picCards = [
     title: "aut ipsam quos ab placeat omnis",
     url: "https://via.placeholder.com/600/602b9e",
     thumbnailUrl: "https://via.placeholder.com/150/602b9e",
+    rating: 2.2,
+    timming: "28 min",
   },
   {
     albumId: 1,
@@ -210,6 +268,8 @@ const picCards = [
     title: "odio enim voluptatem quidem aut nihil illum",
     url: "https://via.placeholder.com/600/372c93",
     thumbnailUrl: "https://via.placeholder.com/150/372c93",
+    rating: 1.5,
+    timming: "10 min",
   },
 ];
 
